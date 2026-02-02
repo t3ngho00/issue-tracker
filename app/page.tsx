35bdb4ee18ece768681fd85ac649841facb0d139
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2026-02-02 19:36:34 +0200

    feat: enhance Pagination component with dynamic page navigation and async props handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Pagination from "@/components/Pagination";
 
-export default function Home() {
-  return <Pagination itemCount={100} pageSize={11} currentPage={1} />;
+interface Props {
+  searchParams: Promise<{ page: string }>;
+}
+export default async function Home({ searchParams }: Props) {
+  const { page } = await searchParams;
+  return (
+    <Pagination itemCount={100} pageSize={11} currentPage={parseInt(page!)} />
+  );
 }
--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Button } from "./ui/button";
 import {
   FaAngleDoubleLeft,
@@ -12,22 +14,46 @@ interface Props {
   pageSize: number;
 }
 const Pagination = ({ itemCount, currentPage, pageSize }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const changePage = (page: number): void => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   const pageCount = Math.ceil(itemCount / pageSize);
-  console.log(pageCount);
   if (pageCount <= 1) return null;
   return (
     <div className="flex items-center gap-2">
       Page {currentPage} of {pageCount}
-      <Button variant={"ghost"} disabled={currentPage == 1}>
+      <Button
+        variant={"ghost"}
+        disabled={currentPage == 1}
+        onClick={() => changePage(1)}
+      >
         <FaAngleDoubleLeft />
       </Button>
-      <Button variant={"ghost"} disabled={currentPage == 1}>
+      <Button
+        variant={"ghost"}
+        disabled={currentPage == 1}
+        onClick={() => changePage(currentPage-1)}
+      >
         <FaAngleLeft />
       </Button>
-      <Button variant={"ghost"} disabled={currentPage == pageCount}>
+      <Button
+        variant={"ghost"}
+        disabled={currentPage == pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <FaAngleRight />
       </Button>
-      <Button variant={"ghost"} disabled={currentPage == pageCount}>
+      <Button
+        variant={"ghost"}
+        disabled={currentPage == pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <FaAngleDoubleRight />
       </Button>
     </div>
