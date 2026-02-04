03810417782d47d582e0d54072ebafcd9666a5fc
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2026-02-04 15:10:45 +0200

    feat: integrate Prisma Postgres for database hosting

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,13 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../app/generated/prisma/client";  // Corrected path
+import { PrismaClient } from "../app/generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    accelerateUrl: process.env.DATABASE_URL!,
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
